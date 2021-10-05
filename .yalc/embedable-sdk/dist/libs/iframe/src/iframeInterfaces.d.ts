ead7005e41d691532fd40e59396e130b87c06b6d
Laura Campbell <lmmcampbell@gmail.com>
2021-10-05 10:11:51 -0500

    Revert "feat: updated for making repo public"
    This reverts commit 7f70adcaff187d1cd9dcfc4ae1f6f0ef668f02cf.
    
    undo push to main instead of branch
     Please enter the commit message for your changes. Lines starting

---
+++ .yalc/embedable-sdk/dist/libs/iframe/src/iframeInterfaces.d.ts
@@ -0,0 +1,22 @@
+export interface ReportIframeInterface {
+    token: string;
+    reportId: string;
+    container: HTMLElement;
+    domain: string;
+    width?: string;
+    height?: string;
+    fullScreen?: boolean;
+}
+export interface AccountDisclosuresIframeInterface {
+    token: string;
+    accountId: string;
+    container: HTMLElement;
+    domain: string;
+    width?: string;
+    height?: string;
+    fullScreen?: boolean;
+}
+export interface CandidateReportIframeInterface {
+    iframe: HTMLIFrameElement;
+}
+//# sourceMappingURL=iframeInterfaces.d.ts.map
